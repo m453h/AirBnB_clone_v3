28d16281d50ae33037ca1d9c2327aefc2a23e3b8
Tobi Tijani <tobi_tijani@yahoo.com>
2023-07-29 11:12:20 -0700

    Task 9

---
+++ api/v1/views/users.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""
+users.py
+This module creates a new view for User objects that
+handles all default RESTFul API actions
+"""
+
+from flask import jsonify, abort, request
+from api.v1.views import app_views
+from models import storage
+from models.state import User
+
+
+@app_views.route("/users", methods=["GET"])
+def list_users():
+    """Retrieves the list of all User objects"""
+    user_objs = storage.all(User)
+    users_list = []
+    for user in user_objs.values():
+        users_list.append(user.to_dict())
+    return jsonify(users_list)
+
+
+@app_views.route("/users/<user_id>", methods=["GET"])
+def get_user(user_id):
+    """Retrieves a User object by id"""
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+
+    return jsonify(user.to_dict())
+
+
+@app_views.route("/users/<user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    """Deletes a User object by id"""
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    user.delete()
+    storage.save()
+    return jsonify({})
+
+
+@app_views.route("/users", methods=["POST"])
+def create_user():
+    """Creates a new User and stores it"""
+    user_data = request.get_json()
+    if not user_data:
+        abort(400, "Not a JSON")
+    if 'name' not in user_data:
+        abort(400, "Missing name")
+    user = User(**user_data)
+    user.save()
+    return jsonify(user.to_dict()), 201
+
+
+@app_views.route("/users/<user_id>", methods=["PUT"])
+def update_user(user_id):
+    """Updates a User given by user_id and stores it"""
+    user = storage.get(User, user_id)
+
+    if user is None:
+        abort(404)
+    if len(request.data) == 0:
+        abort(400, "Not a JSON")
+    user_data = request.get_json()
+    if not user_data:
+        abort(400, "Not a JSON")
+    for key, value in user_data.items():
+        keys_to_ignore = ["id", "created_at", "updated_at"]
+        if key not in keys_to_ignore:
+            setattr(user, key, value)
+    user.save()
+    user = user.to_dict()
+    return jsonify(user), 200
